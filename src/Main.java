d551c63f13baa7f6b4ba9ffade34ac1ce54378bd
Stefan <stefan.piotter@gmx.de>
2017-04-03 13:10:01 +0200

    Neues

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Alles Klar?");
         //Das ist ein Kommentar
-
+        //Das ist neu
         /*Das ist
           noch ein Kommentar
          */
