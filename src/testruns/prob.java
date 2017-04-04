284cb690bc6bb01980337e4cf752c0e81bba4f8c
Andi <andreas.grzenia@hs-bochum.de>
2017-04-04 13:39:53 +0200

    andi macht probe

--- src/testruns/prob.java
+++ src/testruns/prob.java
@@ -3,9 +3,23 @@ package testruns;
 /**
  * Created by Andi on 03.04.2017.
  */
+
+import com.aldebaran.qi.Application;
+import com.aldebaran.qi.helper.proxies.ALTextToSpeech;
+
 public class prob {
     public static void main(String[] args){
-        //test test
-        System.out.println("Hallo Jungs und Mädels");
+        Application application = new Application(args);
+        try{
+            // Start the application and create a session.
+            application.start();
+            // A session has been created. It can be retrieved this way:
+            // application.session();
+        }
+        catch(Exception e){
+            // The application could not be started.
+            e.printStackTrace();
+        }}
+
     }
 }
