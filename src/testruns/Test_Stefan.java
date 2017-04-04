ceaad827f9d47f542039f6407a6a91e876d4a1cb
Stefan <stefan.piotter@gmx.de>
2017-04-04 14:11:13 +0200

    Neue Lib

--- src/testruns/Test_Stefan.java
+++ src/testruns/Test_Stefan.java
@@ -2,22 +2,24 @@ package testruns; /**
  * Created by JNaoHSBO on 04.04.2017.
  */
 
-    import com.aldebaran.qi.*;
+    import com.aldebaran.qi.Application;
+    import com.aldebaran.qi.helper.proxies.ALTextToSpeech;;
+
 
 public class Test_Stefan {
 
-        public static void main(String[] args){
-            Application application = new Application(args);
-            try{
-                // Start the application and create a session.
-                application.start();
-                // A session has been created. It can be retrieved this way:
-                // application.session();
-            }
-            catch(Exception e){
-                // The application could not be started.
-                e.printStackTrace();
-            }
+    public static class SayHello {
 
+        public static void main(String[] args) throws Exception {
+            String robotUrl = "tcp://nao.local:9559";
+            // Create a new application
+            Application application = new Application(args, robotUrl);
+            // Start your application
+            application.start();
+            // Create an ALTextToSpeech object and link it to your current session
+            ALTextToSpeech tts = new ALTextToSpeech(application.session());
+            // Make your robot say something
+            tts.say("Hello World!");
         }
+    }
 }
