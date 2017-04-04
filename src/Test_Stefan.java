0e1bc9505d8b782ab8ee61f203155162a62d8428
Stefan <stefan.piotter@gmx.de>
2017-04-04 11:43:07 +0200

    Isker -> fix stefan_test -> neu

---
+++ src/Test_Stefan.java
@@ -0,0 +1,23 @@
+/**
+ * Created by JNaoHSBO on 04.04.2017.
+ */
+
+    import com.aldebaran.qi.*;
+
+public class Test_Stefan {
+
+        public static void main(String[] args){
+            Application application = new Application(args);
+            try{
+                // Start the application and create a session.
+                application.start();
+                // A session has been created. It can be retrieved this way:
+                // application.session();
+            }
+            catch(Exception e){
+                // The application could not be started.
+                e.printStackTrace();
+            }
+
+        }
+}
