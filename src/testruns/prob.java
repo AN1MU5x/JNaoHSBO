72eac9e5a20737074cda6366e42a0edf425f7314
Andi <andreas.grzenia@hs-bochum.de>
2017-04-05 09:40:11 +0200

    andi macht probe

--- src/testruns/prob.java
+++ src/testruns/prob.java
@@ -6,8 +6,22 @@ package testruns;
 
 import com.aldebaran.qi.Application;
 
+import com.aldebaran.qi.helper.proxies.ALTextToSpeech;
+
 public class prob {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
+        String robotUrl = "tcp://nao.local:9559";
+        // Create a new application
+        Application application = new Application(args, robotUrl);
+        // Start your application
+        application.start();
+        // Create an ALTextToSpeech object and link it to your current session
+        ALTextToSpeech tts = new ALTextToSpeech(application.session());
+        // Make your robot say something
+        tts.say("Hello World!");
+    }
+   /* public static void main(String[] args){
+
         Application application = new Application(args);
         try{
             // Start the application and create a session.
@@ -19,7 +33,9 @@ public class prob {
             // The application could not be started.
             e.printStackTrace();
         }
-    }
+        */
 
 }
+
+
 
