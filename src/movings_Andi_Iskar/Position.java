0641e2bac930002ec97b6f9df5a240ae88238452
Andi <andreas.grzenia@hs-bochum.de>
2017-05-03 12:35:12 +0200

    Andi Position

--- src/movings_Andi_Iskar/Position.java
+++ src/movings_Andi_Iskar/Position.java
@@ -24,6 +24,15 @@ public class Position  {
     private static ArrayList angles1;
     private static ArrayList time;
     private static ArrayList time1;
+    private static ArrayList array;
+    private static ArrayList array1;
+    private static ArrayList array2;
+    private static ArrayList array3;
+    private static ArrayList array4;
+    private static ArrayList array5;
+    private static ArrayList array6;
+    private static ArrayList array7;
+
 
 
 
@@ -185,6 +194,47 @@ public class Position  {
         fractionMaxSpeed – The fraction of maximum speed to use
         */
 
+    }
+
+    public static ArrayList moveconfigvor() throws Exception{
+        array = new ArrayList<>();
+        array1 = new ArrayList<>();
+        array2 = new ArrayList<>();
+        array3 = new ArrayList<>();
+        array4 = new ArrayList<>();
+        array5 = new ArrayList<>();
+        array6 = new ArrayList<>();
+        array7 = new ArrayList<>();
+        array1.add(0, "MaxStepX");
+        array1.add(1, 0.08f);
+
+        array2.add(0, "MaxStepY");
+        array2.add(1, 0.16f);
+
+        array3.add(0, "MaxStepTheta");
+        array3.add(1, 0.5f);
+
+        array4.add(0, "MaxStepFrequency");
+        array4.add(1, 0.0f);
+
+        array5.add(0, "StepHeight");
+        array5.add(1, 0.08f);
+        array6.add(0, "TorsoWx");
+        array6.add(1, 0.f);
+
+        array7.add(0, "TorsoWy");
+        array7.add(1, -0.1f);
+
+        array.add(0, array1);
+        array.add(1, array2);
+        array.add(2, array3);
+        array.add(3, array4);
+        array.add(4, array5);
+        array.add(5, array6);
+        array.add(6, array7);
+
+
+        return array;
     }
 }
 
--- src/testruns/Test_mov_Andi.java
+++ src/testruns/Test_mov_Andi.java
@@ -3,28 +3,81 @@ package testruns;
 
 
 
+
 /**
  * Created by Andi on 06.04.2017.
  */
 
 
 
-    import com.aldebaran.qi.helper.proxies.ALLocalization;
-    import com.aldebaran.qi.helper.proxies.ALTextToSpeech;
+    import com.aldebaran.qi.helper.proxies.*;
+    import movings_Andi_Iskar.Position;
     import utillities.Utts;
 
     import java.util.*;
 
 public class Test_mov_Andi {
 
-        public static void main(String[] args) throws Exception {
-            Utts.AppStart();
-            ALLocalization home = new ALLocalization(Utts.getAPP().session());
+    public static void main(String[] args) throws Exception {
+        Utts.AppStart();
+        ALLocalization home = new ALLocalization(Utts.getAPP().session());
 
-           // home.learnHome();
+        ALMotion bewe = new ALMotion(Utts.getSESSION());
+        ALNavigation be = new ALNavigation(Utts.getSESSION());
+
+        ArrayList array = new ArrayList<>();
+        ArrayList array1 = new ArrayList<>();
+        ArrayList array2 = new ArrayList<>();
+        ArrayList array3 = new ArrayList<>();
+        ArrayList array4 = new ArrayList<>();
+        ArrayList array5 = new ArrayList<>();
+        ArrayList array6 = new ArrayList<>();
+        ArrayList array7 = new ArrayList<>();
+        array1.add(0, "MaxStepX");
+        array1.add(1, 0.08f);
+
+        array2.add(0, "MaxStepY");
+        array2.add(1, 0.16f);
+
+        array3.add(0, "MaxStepTheta");
+        array3.add(1, 0.5f);
+
+        array4.add(0, "MaxStepFrequency");
+        array4.add(1, 0.0f);
+
+        array5.add(0, "StepHeight");
+        array5.add(1, 0.08f);
+        array6.add(0, "TorsoWx");
+        array6.add(1, 0.f);
+
+        array7.add(0, "TorsoWy");
+        array7.add(1, -0.1f);
+
+        array.add(0, array1);
+        array.add(1, array2);
+        array.add(2, array3);
+        array.add(3, array4);
+        array.add(4, array5);
+        array.add(5, array6);
+        array.add(6, array7);
+        System.out.println(array);
+        System.out.println(bewe.getMoveConfig("Max"));
+        System.out.println(bewe.getMoveConfig("Min"));
+        System.out.println(bewe.getMoveConfig("Default"));
+        bewe.moveTo(0.5f, 0.f, 0.f, Position.moveconfigvor());
 
 
-            ArrayList<Float> homepostion = new ArrayList<>();
+
+        //home.
+
+       // bewe.stopMove();
+        // be.move(0.5f,0f,0f);
+
+
+        // home.learnHome();
+
+
+           /* ArrayList<Float> homepostion = new ArrayList<>();
             ArrayList<Float> prob = new ArrayList<>();
            homepostion=(ArrayList)home.getRobotPosition();
            prob.add(0,0f);
@@ -48,9 +101,10 @@ public class Test_mov_Andi {
             System.out.println("Roboter Postionn nach go homer "+home.getRobotPosition());
             System.out.println();
 
-        }
+        }*/
 
     }
+}
 
 
 
